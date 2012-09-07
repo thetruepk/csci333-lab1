a452d6d241fcf9e4427717f73514ef4412c82daa
thetruepk <jlmccask@unca.edu>
2012-09-07 05:21:17 -0400

    First commit of the 2nd Euler project

---
+++ projectEuler002.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(){
+  int nOne;
+  int nTwo;
+  int limit;
+  int placeMarker;
+  int sum;
+
+  nOne = 0;
+  nTwo = 1;
+  limit = 987;
+  placeMarker = 0;
+  sum = 0;
+
+ while(sum < limit){
+   if(nTwo%2==0){
+     placeMarker+= sum;
+   }
+   sum = nOne + nTwo;
+   nOne = nTwo;
+   nTwo = sum;
+    
+   cout<<placeMarker << endl;
+
+
+
+
+
+
+
+
+  }
+
+}
